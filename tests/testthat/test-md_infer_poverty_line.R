c2e27b8f406cde88a504f840fc910ca1be20fae2
R.Andres Castaneda <acastanedaa@worldbank.org>
2020-10-09 15:30:32 -0400

    first tests

--- tests/testthat/test-md_infer_poverty_line.R
+++ tests/testthat/test-md_infer_poverty_line.R
@@ -1,3 +1,17 @@
+
+context("uniform distribution of welfare")
 test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+  set.seed(10010)
+  uv   <- 5
+  ps   <- runif(1)
+  size <- 1001
+  expect_equal(md_infer_poverty_line(rep(uv, size),
+                                     popshare = ps),
+               uv)
+
+  expect_equal(md_infer_poverty_line(rep(uv, size),
+                                     sample(1:500, size, replace = TRUE),
+                                     popshare = ps),
+               uv)
+
 })
