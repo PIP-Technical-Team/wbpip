119c58e429b3ac28aa28153f67e8160095dbebc6
Ifeanyi Edochie <ifeanyi.edochie@gmail.com>
2020-10-10 21:37:50 -0400

    no big changes, just removed the na.rm arguments and translated the poverty gap, severity and watts index into divide by population size.

--- R/md_compute_poverty_stats.R
+++ R/md_compute_poverty_stats.R
@@ -1,9 +1,46 @@
+#' Compute Poverty Statistics
+#'
+#' Compute the poverty headcount, poverty gap, poverty severity and
+#' the watts index for a micro data set.
+#'
+#' Given a vector of consumption or income values and their respective weights
+#' `md_compute_poverty_stats()` computes poverty headcount, poverty gap,
+#' poverty severity and the watts index.
+#'
+#' Negative values are dropped from computations.
+#'
+#' @param welfare numeric: A vector of income or consumption values
+#' @param povline numeric: A poverty line/threshold
+#' @param weight numeric: A vector of weights, optional, a vector of 1s if not specified.
+#'
+#'
+#' @return List object containing poverty headcount, poverty gap, poverty severity and the watts index
+#' @export
+#'
+#' @examples
+#' # simple example (no weights)
+#' md_compute_poverty_stats(welfare = 1:100, povline = 10)
+#'
+#' # with weights
+#' md_compute_poverty_stats(welfare = 1:100, povline = 10, weight = 1:100)
+#'
+#' # using a micro data set
+#' data("md_ABC_2000_income")
+#' md_compute_poverty_stats(md_ABC_2000_income$welfare, 2500000, md_ABC_2000_income$weight)
+#'
+#'
 md_compute_poverty_stats <- function(welfare, povline, weight = NULL) {
 
   ## set all weights to 1 if none are supplied
   if (is.null(weight) == TRUE) {
     weight <- rep(1, length(welfare))
   }
+
+  ## check inputs and clean up for analysis
+  md_check_inputs(welfare = welfare, weight = weight)
+
+  res <- md_clean_inputs(welfare = welfare, weight = weight)
+  welfare <- res$welfare; weight <- res$weight
 
   headcount <- 0
   gap <- 0
@@ -19,16 +56,22 @@ md_compute_poverty_stats <- function(welfare, povline, weight = NULL) {
 
       #in this block I set na.rm = TRUE for sums to ensure that in the presence of na.rm function
       #wont return missing values
-      headcount <- sum(headcount, weight_i, na.rm = TRUE)
+      headcount <- sum(headcount, weight_i)
       gap_i <- 1 - welfare_i / povline
-      gap <- sum(gap, weight_i * gap_i, na.rm = TRUE)
+      gap <- sum(gap, weight_i * gap_i)
       severity <- sum(severity, weight_i * gap_i ^ 2)
       if (welfare_i > 0) { # Is this check needed no negative welfare value should make it to the application
-        watt8 <- sum(watt8, weight_i * log(povline / welfare_i), na.rm = TRUE)
+        watt8 <- sum(watt8, weight_i * log(povline / welfare_i))
       }
 
     }
   }
+
+  #compute the values for the return
+  gap <- gap / sum(weight)
+  severity <- severity / sum(weight)
+  watt8 <- watt8 / sum(weight)
+
 
   return(list(
     headcount = headcount,
