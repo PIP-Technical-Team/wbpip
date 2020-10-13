411e5d29deff6f88daca5ee204d848b81d6fe1f7
Tony Fujs <tfujs@worldbank.org>
2020-10-13 19:37:42 +0200

    add create_functional_form_lq()

---
+++ R/gd_estimate_lq.R
@@ -0,0 +1,45 @@
+#' Prepares data for Lorenz Quadratic regression
+#'
+#' @description  Prepares data for regression on L(1-L) on (P^2-L), L(P-1) and
+#' (P-L). The last observation of (p,l), which by construction has the value
+#' (1, 1), is excluded since the functional form for the Lorenz curve already
+#' forces it to pass through the point (1, 1). Equation 15 in Lorenz Quadratic
+#' original paper.
+#'
+#' @param lorenz_pop numeric: Population vector from empirical Lorenz curve
+#' @param lorenz_welfare numeric: Welfare vector from empirical Lorenz curve
+#'
+#' @return data.frame
+#'
+#' @seealso \href{https://EconPapers.repec.org/RePEc:eee:econom:v:40:y:1989:i:2:p:327-338}{Original quadratic Lorenz curve paper}
+#' @seealso \href{https://www.sciencedirect.com/science/article/abs/pii/S0304407613000158?via%3Dihub}{Corrigendum to Elliptical Lorenz Curves}
+
+create_functional_form_lq <- function(lorenz_pop,
+                                      lorenz_welfare) {
+  # CHECK inputs
+  assertthat::assert_that(is.numeric(lorenz_pop))
+  assertthat::assert_that(is.numeric(lorenz_welfare))
+  assertthat::assert_that(length(lorenz_pop) == length(lorenz_welfare))
+  assertthat::assert_that(length(lorenz_pop) > 1)
+
+  # Remove last observation (the functional form for the Lorenz curve already forces
+  # it to pass through the point (1, 1)
+  nobs <- length(lorenz_pop) - 1
+  lorenz_pop <- lorenz_pop[1:nobs]
+  lorenz_welfare <- lorenz_welfare[1:nobs]
+
+  # L(1-L)
+  y <- lorenz_welfare * (1 - lorenz_welfare)
+  # (P^2-L)
+  x1 <- lorenz_pop^2 - lorenz_welfare
+  # L(P-1)
+  x2 <- lorenz_welfare * (lorenz_pop - 1)
+  # P-L
+  x3 <- lorenz_pop - lorenz_welfare
+
+  out <- data.frame(y, x1, x2, x3, stringsAsFactors = FALSE)
+
+  return(out)
+}
+
+
