05aa099987a3c149d25b86e6836650d2fb00d2b0
R.Andres Castaneda <acastanedaa@worldbank.org>
2021-02-26 16:57:38 -0500

    create file for old way to make calculations

---
+++ R/md_compute_poverty_stats_old.R
@@ -0,0 +1,50 @@
+#' Compute Poverty Statistics
+#'
+#' Compute poverty statictics for microdata.
+#'
+#' Given a vector of consumption or income values and their respective weights
+#' `md_compute_poverty_stats()` computes poverty headcount, poverty gap,
+#' poverty severity and the watts index.
+#'
+#' @param welfare numeric: A vector of income or consumption values.
+#' @param weight numeric: A vector of weights.
+#' @param povline_lcu numeric: Poverty line in Local Currency Unit (LCU).
+#'
+#' @examples
+#' wbpip:::md_compute_poverty_stats(
+#'   welfare = 1:2000,
+#'   weight = rep(1, 2000),
+#'   povline_lcu = 10)
+#'
+#' @return list
+#' @keywords internal
+md_compute_poverty_stats <- function(welfare, weight, povline_lcu) {
+
+  alpha             <- c(0, 1, 2)
+  pov_status        <- data.table::fifelse(welfare < povline_lcu, 1, 0)
+
+  relative_distance <- (1 - (welfare / povline_lcu))
+
+  #--------- FGT measures ---------
+
+  for (a in seq_along(alpha)) {
+
+    y <- paste0("fgt", alpha[a])
+    fgt <-  pov_status*relative_distance^alpha[a]
+    x <- collapse::fmean(x = fgt, w = weight)
+    assign(y, x)
+
+  }
+
+  #--------- Watts index ---------
+
+  sensitive_distance <- log(povline_lcu / welfare)*pov_status
+  watts              <- collapse::fmean(x = sensitive_distance, w = weight)
+
+  return(list(
+    headcount = headcount,
+    poverty_gap = gap,
+    poverty_severity = severity,
+    watts = watt8
+  ))
+}
