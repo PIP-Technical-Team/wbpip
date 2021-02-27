0338132babba5f008cd55afca5ee8a2e666b80ed
R.Andres Castaneda <acastanedaa@worldbank.org>
2021-02-27 12:12:41 -0500

    Add proper labels in tests instead of comments

--- tests/testthat/test-md_compute_poverty_stats.R
+++ tests/testthat/test-md_compute_poverty_stats.R
@@ -1,38 +1,51 @@
 benchmark <- readRDS('../testdata/synthetic-microdata.RDS')
+# benchmark <- readRDS('tests/testdata/synthetic-microdata.RDS')
 benchmark <- benchmark[[1]]$data
 
-test_that('md_compute_poverty_stats() matched expected results', {
+context('md_compute_poverty_stats() matched expected results')
+test_that('does function return 0 headcount when all welfare is above poverty line?', {
 
-  # does function return 0 headcount when all welfare is above poverty line?
   res <- md_compute_poverty_stats(welfare = 10:100, povline_lcu = 9, weight = 10:100)
   expect_equal(
     res$headcount,
     0
   )
-
-  # does function return all as poor when all welfare values are below poverty line?
+})
+test_that('does function return all as poor when all welfare values are below poverty line?'
+          , {
   pop <- 1:100
   res <- md_compute_poverty_stats(welfare = pop, povline_lcu = 101, weight = rep(1, 100))
   expect_equal(
     res$headcount,
     1
   )
+})
+test_that('does poverty gap = 1 when welfare values are 0?'
+          , {
 
-  # does poverty gap = 1 when welfare values are 0?
-  res <- md_compute_poverty_stats(welfare = rep(0, 10), povline_lcu = 12, weight = rep(1, 10))
+  res <- md_compute_poverty_stats(welfare = rep(0, 10),
+                                  povline_lcu = 12,
+                                  weight = rep(1, 10))
   expect_equal(
     res$poverty_gap,
     1
   )
+})
 
-  # does poverty gap = 0 when welfare values are at least the povline?
-  res <- md_compute_poverty_stats(welfare = 21:30, povline_lcu = 15, weight = rep(1, 10))
+test_that('does poverty gap = 0 when welfare values are at least the povline?'
+          , {
+
+  res <- md_compute_poverty_stats(welfare = 21:30,
+                                  povline_lcu = 15,
+                                  weight = rep(1, 10))
   expect_equal(
     res$poverty_gap,
     0
   )
+})
 
-  # does function produce results that match compute_poverty_stats in povcalnet
+test_that('does function produce results that match compute_poverty_stats in povcalnet'
+          , {
   res <- md_compute_poverty_stats(welfare = benchmark$welfare,
                                   povline_lcu = mean(benchmark$welfare),
                                   weight = benchmark$weight)
