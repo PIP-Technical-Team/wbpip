1b4373506fee047ce5b8d4ff5c5e92786774f0f0
R.Andres Castaneda <acastanedaa@worldbank.org>
2021-03-02 14:23:35 -0500

    compiled Gini

--- R/md_compute_gini.R
+++ R/md_compute_gini.R
@@ -1,19 +1,9 @@
-#' Gini coefficient
+#' Not compiled Gini
 #'
-#' Compute the Gini coefficient for microdata.
+#' @inheritParams md_compute_gini
 #'
-#' Given a vector of income or consumption values and their respective weights
-#' `md_compute_gini()` computes the Gini coefficient for the distribution.
-#'
-#' @param welfare numeric: A vector of income or consumption values.
-#' @param weight numeric: A vector of weights.
-#'
-#' @examples
-#' wbpip:::md_compute_gini(welfare = 1:2000, weight = rep(1, 2000))
-#'
-#' @return numeric
-#' @keywords internal
-md_compute_gini <- function(welfare, weight) {
+#' @return scalar
+fgini <- function(welfare, weight) {
 
   # Compute weighted welfare
   weighted_welfare <- welfare * weight
@@ -33,4 +23,46 @@ md_compute_gini <- function(welfare, weight) {
 
   return(gini)
 }
+
+#' Gini coefficient
+#'
+#' Compute the Gini coefficient for microdata.
+#'
+#' Given a vector of income or consumption values and their respective weights
+#' `md_compute_gini()` computes the Gini coefficient for the distribution.
+#'
+#' @param welfare numeric: A vector of income or consumption values.
+#' @param weight numeric: A vector of weights.
+#'
+#' @examples
+#' wbpip:::md_compute_gini(welfare = 1:2000, weight = rep(1, 2000))
+#'
+#' @return numeric
+#' @keywords internal
+md_compute_gini <- compiler::cmpfun(fgini)
+
+
+# md_compute_gini <- function(welfare, weight) {
+#
+#   # Compute weighted welfare
+#   weighted_welfare <- welfare * weight
+#   weighted_welfare_lag <- collapse::flag(weighted_welfare, fill = 0)
+#
+#   # Compute area under the curve using
+#   # Area of trapezoid = Base * Average height
+#   v <- (cumsum(weighted_welfare_lag) + (weighted_welfare / 2) ) * weight
+#   auc <- collapse::fsum(v) # Area Under the Curve
+#
+#   # Compute Area Under the Lorenz Curve
+#   # Normalize auc so it is always between 0 and 0.5
+#   auc <- (auc / collapse::fsum(weight)) / collapse::fsum(weighted_welfare)
+#
+#   # Compute Gini
+#   gini <- 1 -  (2 * auc)
+#
+#   return(gini)
+# }
+
+
+
 
