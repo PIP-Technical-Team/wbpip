b0d9132ec4fb9196a041659fcdf20749ea4ed57d
Ifeanyi Edochie <ifeanyi.edochie@gmail.com>
2021-03-17 09:14:49 -0400

    a test for the get_bins function, original get_bins() function likely needs to be updated to remove error message received when group data is specified

--- tests/testthat/test-get_bins.R
+++ tests/testthat/test-get_bins.R
@@ -1,3 +1,21 @@
-# test_that("multiplication works", {
-#   expect_equal(2 * 2, 4)
-# })
+# testing the get bins function
+
+
+test_that("get bins produces expected outputs", {
+
+  # Read in synthetic microdata
+  dl <- readRDS('../testdata/synthetic-microdata.RDS')
+
+  dl <- dl[[1]]$data
+
+  bins <- get_bins(dl, welfare = welfare, weight = weight)
+
+  #test that the correct number of bins
+  testthat::expect_equal(length(unique(bins$bins)), 100)
+
+  #I cant really test what happens when distribution type is "group" because it also kicks out an error message
+
+  ## "Error in get_bins(dl, welfare = welfare, weight = weight, distribution_type = "group") :
+  ## object 'df_bins' not found"
+
+})
