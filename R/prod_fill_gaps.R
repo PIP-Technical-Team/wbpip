9fb30b816b691f9f66fbc42657612cf014c81921
Tony Fujs <tfujs@worldbank.org>
2021-05-04 16:38:54 +0200

    fix typo

--- R/prod_fill_gaps.R
+++ R/prod_fill_gaps.R
@@ -55,7 +55,7 @@ prod_fg_compute_pip_stats <- function(request_year,
     svy_mean_lcu = svy_mean_lcu,
     data = data,
     poverty_line = poverty_line,
-    popshare = poshare,
+    popshare = popshare,
     default_ppp = default_ppp,
     ppp = ppp,
     type = type
