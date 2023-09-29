6475bd5b9c5482a483efb31a23334ad34e13269f
Andres <acastastanedaa@worldbank.org>
2023-09-29 18:06:24 -0400

    import collapse

--- R/wbpip-package.R
+++ R/wbpip-package.R
@@ -10,31 +10,31 @@
 #'
 #' @docType package
 #' @name wbpip
-# @import data.table
 #'
 # Make sure data.table knows we know we're using it
-#' @noRd
-# .datatable.aware = TRUE
+#' .datatable.aware = TRUE
 
 # Prevent R CMD check from complaining about the use of pipe expressions
 # standard data.table variables
-if (getRversion() >= "2.15.1") {
-  utils::globalVariables(
-    names = c(
-      ".",
-      ".I",
-      ".N",
-      ".SD",
-      ".",
-      "!!",
-      ":=",
-      "..output"
-    ),
-    package = utils::packageName()
-  )
-}
+# if (getRversion() >= "2.15.1") {
+#   utils::globalVariables(
+#     names = c(
+#       ".",
+#       ".I",
+#       ".N",
+#       ".SD",
+#       ".",
+#       "!!",
+#       ":=",
+#       "..output"
+#     ),
+#     package = utils::packageName()
+#   )
+# }
 
 ## usethis namespace: start
 #' @importFrom lifecycle deprecated
+#' @import collapse
 ## usethis namespace: end
+#' @noRd
 NULL
