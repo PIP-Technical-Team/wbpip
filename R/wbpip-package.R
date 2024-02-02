b85ff9e05cf9bab7c00183e056a9ff06b1145ff4
R.Andres Castaneda Aguilar <acastanedaa@worldbank.org>
2024-02-01 19:19:18 -0500

    add lifecycle images

---
+++ R/wbpip-package.R
@@ -0,0 +1,42 @@
+#' wbpip: Methodological worlkflow of PIP
+#'
+#' Basic workflows are based on single data files (micro- or grouped-data). The
+#' assumption for these workflows is that they can be satisfied by feeding a
+#' single micro- / grouped-data file to different functions
+#'
+#' @section wbpip functions: The wbpip are divided in...
+#'
+#' @docType package
+#' @name wbpip
+#' @import data.table
+#' @import collapse
+#' @importFrom cli cli_abort cli_warn cli_inform cli_alert cli_alert_danger
+#'   cli_alert_info cli_alert_success
+
+# Make sure data.table knows we know we're using it
+#' @noRd
+.datatable.aware = TRUE
+
+# Prevent R CMD check from complaining about the use of pipe expressions
+# standard data.table variables
+if (getRversion() >= "2.15.1") {
+  utils::globalVariables(
+    names = c(
+      ".",
+      ".I",
+      ".N",
+      ".SD",
+      ".",
+      "!!",
+      ":="
+    ),
+    package = utils::packageName()
+  )
+}
+
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
