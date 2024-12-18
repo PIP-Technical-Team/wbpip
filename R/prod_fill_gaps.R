2078bbe00e8db77b64bc4445ff29551c3eeee546
shahronak47 <shahronak47@yahoo.in>
2024-12-18 06:32:13 +0530

    rm browser

--- R/prod_fill_gaps.R
+++ R/prod_fill_gaps.R
@@ -26,7 +26,6 @@ prod_fg_compute_pip_stats <- function(request_year,
                                       distribution_type,
                                       poverty_line,
                                       popshare) {
-  browser()
 
   # Set type
   type <- distribution_type
